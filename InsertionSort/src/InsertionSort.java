5c1c06bc0b21442c6a9e2dc594ff164aa57880ca
bredenhulse <bredenhulse@gmail.com>
2022-01-25 07:59:36 -0600

    Yes

--- InsertionSort/src/InsertionSort.java
+++ InsertionSort/src/InsertionSort.java
@@ -8,10 +8,17 @@ public class InsertionSort {
 				numbers.add(scan.nextInt());
 			}
 		for(int i = 1; i < numbers.size(); i++){
-			for(int j = i; numbers.get(j) < numbers.get(j - 1); j++){
+			for(int j = i; j > 0; j--){
+				if(j == 0){
+					break;
+				}
+				else if (numbers.get(j) < numbers.get(j - 1)){
 				int temp = numbers.get(j);
 				numbers.set(j, numbers.get(j -1));
 				numbers.set(j - 1, temp);
+				}
+				else{
+				}
 			}
 
 		}
